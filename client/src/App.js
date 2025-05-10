c3a4e1babd17c67c3c315613adf54888354edff7
manikanta70137 <141338314+manikanta70137@users.noreply.github.com>
2025-05-10 14:48:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import JoinRoom from './components/JoinRoom';
+import PollRoom from './components/PollRoom';
 
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<JoinRoom />} />
+        <Route path="/room/:id" element={<PollRoom />} />
+      </Routes>
+    </Router>
+  );
+}
