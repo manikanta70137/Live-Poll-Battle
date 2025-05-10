ee566e32f96c65517088ff4cdbb599a3a64997dc
manikanta70137 <141338314+manikanta70137@users.noreply.github.com>
2025-05-10 14:29:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,35 +1,72 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+// server/server.js
+const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+const wss = new WebSocket.Server({ port: 8080 });
+const rooms = {};
 
-app.use(cors());
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    const { type, payload } = data;
 
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+    if (type === 'CREATE_ROOM') {
+      const roomId = uuidv4().slice(0, 6);
+      rooms[roomId] = { users: [], votes: {}, ended: false, timer: null };
+      ws.send(JSON.stringify({ type: 'ROOM_CREATED', payload: roomId }));
+    }
 
-  socket.on("createRoom", ({ name }) => {
-    const roomId = Math.random().toString(36).substring(2, 8);
-    socket.join(roomId);
-    socket.emit("roomCreated", roomId);
+    if (type === 'JOIN_ROOM') {
+      const { roomId, name } = payload;
+      if (rooms[roomId] && !rooms[roomId].ended) {
+        rooms[roomId].users.push({ ws, name });
+        ws.roomId = roomId;
+        ws.name = name;
+
+        if (!rooms[roomId].timer) {
+          rooms[roomId].timer = setTimeout(() => {
+            rooms[roomId].ended = true;
+            broadcast(roomId, { type: 'VOTING_ENDED' });
+          }, 60000);
+        }
+
+        broadcast(roomId, {
+          type: 'USER_JOINED',
+          payload: rooms[roomId].votes
+        });
+      }
+    }
+
+    if (type === 'CAST_VOTE') {
+      const { roomId, option } = payload;
+      if (rooms[roomId] && !rooms[roomId].ended) {
+        rooms[roomId].votes[ws.name] = option;
+        const voteCount = countVotes(rooms[roomId].votes);
+        broadcast(roomId, {
+          type: 'VOTE_UPDATE',
+          payload: voteCount
+        });
+      }
+    }
   });
+});
 
-  socket.on("joinRoom", ({ name, roomId }) => {
-    socket.join(roomId);
-    socket.emit("joinedRoom");
+function broadcast(roomId, message) {
+  const room = rooms[roomId];
+  if (room) {
+    room.users.forEach(({ ws }) => {
+      ws.send(JSON.stringify(message));
+    });
+  }
+}
+
+function countVotes(votes) {
+  const tally = { A: 0, B: 0 };
+  Object.values(votes).forEach(vote => {
+    tally[vote]++;
   });
+  return tally;
+}
 
-  // You can add voting logic here later
-});
+console.log("WebSocket server running on ws://localhost:8080");
 
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
-});
